12a679601f43dacb51f826f653580175f9491700
akashgokul <akashgokul@berkeley.edu>
2022-04-19 05:56:33 +0000

    Fixing target net view order

--- model/byol_model.py
+++ model/byol_model.py
@@ -36,15 +36,14 @@ class BYOLModel(torch.nn.Module):
         
         masks = torch.cat([ masks[:,i,:,:,:] for i in range(masks.shape[1])])
         masks = convert_binary_mask(masks)
-        masks_a = masks[:32]
-        masks_b = masks[32:]
+
         # target network forward
         q,pinds = self.predictor(*self.online_network(torch.cat([view1, view2], dim=0),masks,True))
 
         # target network forward
         with torch.no_grad():
             self._update_target_network(mm)
-            target_z, tinds = self.target_network(torch.cat([view2, view1], dim=0),torch.cat([masks_b,masks_a]),True)
+            target_z, tinds = self.target_network(torch.cat([view1, view2], dim=0),masks,True)
             target_z = target_z.detach().clone()
 
         return q, target_z, pinds, tinds
